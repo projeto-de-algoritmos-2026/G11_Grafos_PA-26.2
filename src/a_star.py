ef67e22d2a1a7e375b5a3563ca4a1f43e17a9943
DaviNegreiros <dv.neg1711@gmail.com>
2026-09-06 22:22:41 -0300

    Implementa min_heap própria

--- src/a_star.py
+++ src/a_star.py
@@ -1,4 +1,3 @@
-import heapq # biblioteca para heap
 import math # para sqrt
 
 # função de estimativa para o A*
@@ -9,7 +8,7 @@ def heuristic(a, b):
     return math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
 
 def get_neighbors(maze, cell):
-    x, y = cell   # l, c
+    x, y = cell   # c, l
 
     # l linhas    
     l = len(maze)
@@ -23,11 +22,58 @@ def get_neighbors(maze, cell):
     for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
         new_x = x + dx
         new_y = y + dy
-        if (0 <= new_x < l and 0 <= new_y < c and maze[new_x][new_y] <= 0):
+        if (0 <= new_x < c and 0 <= new_y < l and maze[new_y][new_x] <= 0):
             neighbors.append((new_x, new_y))
 
     #retorna vizinhos
     return neighbors
+
+def criarHeap():
+    heap = [1] # heap[0] sera usado como ultima posição livre, por isso heap[0] = 1
+    return heap
+
+def swap(heap, x, y):
+    temp = heap[x] 
+    heap[x] = heap[y]
+    heap[y] = temp
+
+def shiftup(heap, i):
+    if i <= 1:
+        return
+
+    pai = i // 2
+    if heap[pai] > heap[i]:
+        swap(heap, pai, i)
+        return shiftup(heap, pai)
+
+def heapfy(heap, i):
+    filhoE = i*2
+    filhoD = i*2 + 1
+
+    # heap[0] -> ultima posição livre, logo heap[0] - 1 é a ultima posição ocupada
+    if filhoE > heap[0] - 1:
+        return
+
+    if filhoD > heap[0] - 1:
+        if heap[filhoE] < heap[i]:
+            swap(heap, i, filhoE)
+            return heapfy(heap, filhoE)
+        return
+    
+    if heap[i] > heap[filhoE] or heap[i] > heap[filhoD]:
+        if heap[filhoE] <= heap[filhoD]:
+            swap(heap, i, filhoE)
+            return heapfy(heap, filhoE)
+        else:
+            swap(heap, i, filhoD)
+            return heapfy(heap, filhoD)
+    
+
+def inserirHeap(heap, celula):
+    ultimo = heap[0]
+    heap.append(celula)
+    heap[0] = ultimo + 1 # poderia ser ++ tambem
+    shiftup(heap, ultimo)
 
 def a_star(maze, start, goal):
 
@@ -35,17 +81,18 @@ def a_star(maze, start, goal):
     if(start == goal):
         return [start]
 
-    # m colunas
-    c = len(maze[0])
-    # n linhas
+    # l linhas    
     l = len(maze)
+    # c colunas
+    c = len(maze[0])
 
     # gaol 
     gx, gy = goal
 
-    # vê se está dentro do labirinto
+    # vê se está dentro do labirinto ou é parede
     if not (0 <= gx < c and 0 <= gy < l) or maze[gy][gx] > 0:
         return []
+
 
     
 #TODO: finalizar implementação do algoritmo A estrela
